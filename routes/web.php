ee160f0450c843244d79502fd7fcfe8cd233d4b4
Abdelaziz <techproduct@hotmail.com>
2020-09-08 13:50:09 +0100

    Routes

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PageController@index')->name('home');
+Route::get('/home', 'PageController@index')->name('home');
 
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
